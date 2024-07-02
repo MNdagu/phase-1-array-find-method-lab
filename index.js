c84197c5ba5358b071d45ca026328772caba1404
MNdagu <ndagumark@gmail.com>
2024-07-02 12:32:03 +0300

    final

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(record){
+   
+      let win = record.find((game) => game.result === "W" );
+
+      if (win){
+        return win.year;
+      }else{
+        return undefined;
+      }
+        
+        
+}
+console.log(superbowlWin(record));
